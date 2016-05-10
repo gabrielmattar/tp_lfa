927a4e28e9f3f8ecc5b00032a0f1d06a21fa5a41
Andre <andredornas299@gmail.com>
2016-05-09 22:53:45 -0300

    inicio sintatico

--- src/tp_lfa/Sintatico.java
+++ src/tp_lfa/Sintatico.java
@@ -10,5 +10,23 @@ package tp_lfa;
  * @author AndreDornas
  */
 public class Sintatico {
+    private final Lexico lexico;
+    private Lexema atual;
     
+    public Sintatico(Lexico lexico) {
+        this.lexico = lexico;
+        atual = lexico.nextToken();
+    }
+    
+    private void matchToken(TiposLexicos type) {
+        if(atual.getType() == type){
+            atual = lexico.nextToken();
+        } else {
+            if(atual.getType() == TiposLexicos.FIM_DO_ARQUIVO)
+                System.out.println(lexico.getLinha() + ": Fim de arquivo inesperado ["+atual.getToken()+"]");
+            else 
+                System.out.println(lexico.getLinha() + ": Lexema inesperado ["+atual.getToken()+"]");
+            System.exit(1);
+        }
+    }
 }
