c5f31b427eb40004f0f8d3cd45a46ae1b8ec5279
Gabriel Mattar <gabrielmattar41@gmail.com>
2016-05-16 22:34:14 -0300

    Limpada no codigo + comentarios

--- src/tp_lfa/Main.java
+++ src/tp_lfa/Main.java
@@ -66,41 +66,40 @@ public class Main {
     }
     
     public Maquina produtoAFD(Maquina m1, Maquina m2,  List<String> alfabeto){
+        //Maquina produto
         Maquina m3 = new Maquina("Produto");
         
         //Estados iniciais de m1 e m2 para definir o inicial do produto
-        Estado inicialm1 = m1.getInicial(), inicialm2 = m2.getInicial();
-        
-        EstadoCombinado inicial = new EstadoCombinado(inicialm1, inicialm2);
-        
+        Estado inicialM1 = m1.getInicial();
+        Estado inicialM2 = m2.getInicial();
+        EstadoCombinado inicial = new EstadoCombinado(inicialM1, inicialM2);
+        //Estados auxiliares para percorrer as maquinas
+        EstadoCombinado origem, destino;
         
         //Comecamos adicionando o incial na fila de estados a serem analisados
         Queue <EstadoCombinado> estados = new LinkedList();
         estados.add(inicial);
         
-        EstadoCombinado atual, transitado;
-        
-        m3.insereEstado(inicial.getNomeCombinado(), inicial.getEstadoM1().isFinal(), inicial.getEstadoM2().isFinal());
-            
+        //Maquina m3 recebe o estado inicial e tem o estado inicial definido
+        m3.insereEstado(inicial);
+        m3.setInicial(inicial.getNomeCombinado()); 
         
         while(!estados.isEmpty()){
-            atual = estados.remove();
+            origem = estados.remove();
             for(String simbolo : alfabeto){
-                if((transitado = atual.getTransicao(simbolo)) != null) {
-                    if(m3.insereEstado(transitado.getNomeCombinado(), 
-                            transitado.getEstadoM1().isFinal(), transitado.getEstadoM2().isFinal())){
-                        estados.add(transitado);
-                    }
-                    m3.insereTransicao(atual.getNomeCombinado(),  simbolo, transitado.getNomeCombinado());
+                //Caso a transicao nao va para estado de erro
+                if((destino = origem.getTransicao(simbolo)) != null) {
+                    //Caso a insercao for bem sucedida significa q o estado nao foi visitado antes 
+                    //logo adicionamos ele na fila
+                    if(m3.insereEstado(destino))
+                        estados.add(destino);
+                    //Inserimos a respectiva transicao na maquina produto
+                    m3.insereTransicao(origem.getNomeCombinado(),  simbolo, destino.getNomeCombinado());
                 }   
             }
         }
-        m3.setInicial(inicial.getNomeCombinado()); 
-        
         m3.printMaquina();
         return m3;
     }
-    
-    
-    
+ 
 }
